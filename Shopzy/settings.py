867591db44a68e024e7406b329606940cc92e628
jithin-sunil <jithinsunil2003@gmail.com>
2026-06-24 01:16:44 +0530

    temp

--- Shopzy/settings.py
+++ Shopzy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=2hk2ugqv)&ny%jy_o)!1lcxs-9as+!i04reqlc9qie_v1yxcf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "shopzy-testing.onrender.com",
